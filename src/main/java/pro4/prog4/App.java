a43d435b9c30bd0ccf4fb78330c1d5d8964f0a31
MahantMhalsekar <onlyprivacymcube@gmail.com>
2025-06-04 22:42:42 +0530

    azurepipeline

---
+++ src/main/java/pro4/prog4/App.java
@@ -0,0 +1,13 @@
+package pro4.prog4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!welcome to prog4" );
+    }
+}
